bd5569c609d2e7eac6cbd72f3ee3352d39e485a6
Sehej Jain <sehejjain@outlook.com>
2021-06-28 21:56:20 +0530

    Added Icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Sehej Jain',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: '',
     siteUrl: 'https://sehej.xyz', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@_sehej',
