1c18aa7d67c3a6db566cbaac1c57ea6e64ef84bf
Sehej Jain <sehejjain@outlook.com>
2021-06-29 14:32:09 +0530

    Updates

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,9 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a
+              href="https://drive.google.com/file/d/1qFuxnb_US6e2I5LG-7yYEJVG0SYmSAf9/view?usp=sharing"
+              className="resume-link">
               Resume
             </a>
           </nav>
--- src/components/nav.js
+++ src/components/nav.js
@@ -173,7 +173,11 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a
+      className="resume-button"
+      href="https://drive.google.com/file/d/1qFuxnb_US6e2I5LG-7yYEJVG0SYmSAf9/view?usp=sharing"
+      target="_blank"
+      rel="noopener noreferrer">
       Resume
     </a>
   );
